651d16bf53d1b2f77d0b63db9eaeb5feade8d01d
MuraFederico <federicomura98@gmail.com>
2022-06-29 14:56:41 +0200

    exercise completed with bonus

---
+++ Product.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+    internal class Product
+    {   
+        Random random = new Random();
+        public string Id { get; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Iva { get; set; }
+
+        public Product(string name, decimal price, int iva)
+        {
+            this.Name = name;
+            this.Price = price;
+            this.Iva = iva;
+            this.Id = this.SetId();
+        }
+
+        private string SetId()
+        {
+            string pseudoId = random.Next(1, 999999).ToString();
+            int numberDigits = pseudoId.Length;
+            while (numberDigits < 8)
+            {
+                pseudoId = "0" + pseudoId;
+                numberDigits = pseudoId.Length;
+            }
+            return pseudoId;
+        }
+
+        public decimal IvaPrice()
+        {
+            decimal increment = this.Price * this.Iva / 100;
+            return this.Price + increment;
+        }
+
+        public string FullName()
+        {
+            return this.Id + this.Name;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using csharp_oop_shop;
+
+Product prod1 = new Product("pasta", 0.80m, 5);
+
+
+Console.WriteLine(prod1.Id);
+
+Console.WriteLine(prod1.IvaPrice());
+
+Console.WriteLine(prod1.FullName());
